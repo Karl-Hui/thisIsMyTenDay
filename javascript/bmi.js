2477b49281e20cf241bbd2269fa4d52cab2196b1
Karl-Hui <KarlHui0311@gmail.com>
2021-04-12 09:47:18 +0800

    done2

--- javascript/bmi.js
+++ javascript/bmi.js
@@ -1,24 +1,28 @@
 function BMI() {
-    const h=document.getElementById('h').value;
-    const w=document.getElementById('w').value;
-    if (isNaN(h) || isNaN(w)) {
-        document.getElementById("result").textContent="Please enter Numbers"
-    } 
-    else 
-    {
-    let bmi=w/(h/100*h/100);
-    let bmio=(bmi.toFixed(2));
-    if (bmio >25 && bmio <=30 ) {
-        document.getElementById("result").textContent="Your BMI is " + bmio +", YA BMI of 25 to 29.9 is considered overweight."
+  const h = document.getElementById("h").value;
+  const w = document.getElementById("w").value;
+  if (isNaN(h) || isNaN(w)) {
+    document.getElementById("result").textContent = "Please enter Numbers";
+  } else {
+    let bmi = w / (((h / 100) * h) / 100);
+    let bmio = bmi.toFixed(2);
+    if (bmio > 25 && bmio <= 30) {
+      document.getElementById("result").textContent =
+        "Your BMI is " +
+        bmio +
+        ", YA BMI of 25 to 29.9 is considered overweight.";
     }
-    if ( bmio >30 ) {
-        document.getElementById("result").textContent="Your BMI is " + bmio +", a BMI of 30 and above is considered obese."
+    if (bmio > 30) {
+      document.getElementById("result").textContent =
+        "Your BMI is " + bmio + ", a BMI of 30 and above is considered obese.";
     }
-    if ( bmio >=20 && bmio <=25 ) {
-        document.getElementById("result").textContent="Your BMI is " + bmio +", a BMI of 20-25 is ideal."
+    if (bmio >= 20 && bmio <= 25) {
+      document.getElementById("result").innerHTML =
+        "Your BMI is " + bmio + `. <u>Congrats!</u> a BMI of 20-25 is ideal.`;
     }
-    if ( bmio <20 ) {
-        document.getElementById("result").textContent="Your BMI is " + bmio +", you're a bit underweight."
+    if (bmio < 20) {
+      document.getElementById("result").textContent =
+        "Your BMI is " + bmio + ", you're a bit underweight.";
     }
-    }
+  }
 }
